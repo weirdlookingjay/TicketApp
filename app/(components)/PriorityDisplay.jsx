292966b31be99adb5616ed1848e630df3726ebf2
Joe Clark <weirdlookingjay@live.com>
2023-11-10 13:44:21 -0500

    Created TicketCard and Related components

---
+++ app/(components)/PriorityDisplay.jsx
@@ -0,0 +1,13 @@
+import { faFire } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React from 'react'
+
+const PriorityDisplay = () => {
+  return (
+    <div className='flex justify-start align-baseline'>
+        <FontAwesomeIcon icon={faFire} className='text-red-400' />
+    </div>
+  )
+}
+
+export default PriorityDisplay
